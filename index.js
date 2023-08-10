e67d753518c2d43716621f2cf0a1dfbbdb3d7b00
leonoqr <leonooiqr@gmail.com>
2023-08-10 16:21:21 +0800

    Update code and add descriptions for input
    Why:
    *
    This change addresses the need by:
    *

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Define all factors
 const btnEl = document.getElementById("btn");
 const budget_El = document.getElementById("budget");
 const maxT_El = document.getElementById("maxT");
@@ -6,12 +7,14 @@ const ScanItvl_El = document.getElementById("ScanItvl");
 const CostTime_El = document.getElementById("CostTime");
 const psScanTime_El = document.getElementById("psScanTime");
 const otScanTime_El = document.getElementById("otScanTime");
-const PptCost_El = document.getElementById("PptCost_El");
-const SsnCost_El = document.getElementById("SsnCost_El");
-const maxS_El = document.getElementById("maxS_El");
+const PptCost_El = document.getElementById("PptCost");
+const SsnCost_El = document.getElementById("SsnCost");
+const maxS_El = document.getElementById("maxS");
 const resultEl = document.getElementById("result");
 
+// Define functions
 function getParams() {
+  // load function values from form
   const budgetValue = budget_El.value;
   const maxTValue = maxT_El.value;
   const minTValue = minT_El.value;
@@ -22,56 +25,44 @@ function getParams() {
   const PptCostValue = PptCost_El.value;
   const SsnCostValue = SsnCost_El.value;
   const maxSValue = maxS_El.value;
-
+  // Display error message if any values are blank
   if (budgetValue === "") {
     alert("Please enter a budget");
-  } 
-  else if (maxTValue === "") {
-    alert("Please enter a budget");
-  } 
-  else if (minTValue === "") {
-    alert("Please enter a budget");
-  } 
-  else if (ScanItvlValue === "") {
-    alert("Please enter a budget");
-  } 
-  else if (CostTimeValue === "") {
-    alert("Please enter a budget");
-  } 
-  else if (psScanTimeValue === "") {
-    alert("Please enter a budget");
-  } 
-  else if (otScanTimeValue === "") {
-    alert("Please enter a budget");
-  } 
-  else if (PptCostValue === "") {
-    alert("Please enter a budget");
-  } 
-  else if (SsnCostValue === "") {
-    alert("Please enter a budget");
-  } 
-  else if (SsnCostValue === "") {
-    alert("Please enter a budget");
-  } 
-  else {
-    resultEl.innerText = `Buget is ${budgetValue} and max_t is ${max_tValue}`;
+  } else if (maxTValue === "") {
+    alert("Please enter maximum fMRI scan time");
+  } else if (minTValue === "") {
+    alert("Please enter minimum FMRI scan time");
+  } else if (ScanItvlValue === "") {
+    alert("Please enter intervals which scans are priced at");
+  } else if (CostTimeValue === "") {
+    alert("Please the cost per interval");
+  } else if (psScanTimeValue === "") {
+    alert("Please enter per-session overhead scan time");
+  } else if (otScanTimeValue === "") {
+    alert("Please enter one-time overhead scan time");
+  } else if (PptCostValue === "") {
+    alert("Please cost per participant");
+  } else if (SsnCostValue === "") {
+    alert("Please cost per session");
+  } else if (maxSValue === "") {
+    alert("Please enter maximum scan time per session");
+  } else {
+    const [effScanTime, NumSessions, SessionDuration, fMRITime] = getOptimalParams();
+    resultEl.innerText = `Effective total scan time: ${effScanTime}
+
+            Optimal study design: 
+            ${NumSessions} session(s)
+            ${SessionDuration} min of scanning per session
+            ${fMRITime} min of fMRI per session`;
   }
 }
 
-function getOptimalParams(birthdayValue) {
-  const currentDate = new Date();
-  const birthdayDate = new Date(birthdayValue);
-  let age = currentDate.getFullYear() - birthdayDate.getFullYear();
-  const month = currentDate.getMonth() - birthdayDate.getMonth();
-
-  if (
-    month < 0 ||
-    (month === 0 && currentDate.getDate() < birthdayDate.getDate())
-  ) {
-    age--;
-  }
-
-  return age;
+function getOptimalParams() {
+  const effScanTime = 1;
+  const NumSessions = 2;
+  const SessionDuration = 3;
+  const fMRITime = 4;
+  return [effScanTime, NumSessions, SessionDuration, fMRITime];
 }
 
 btnEl.addEventListener("click", getParams);
